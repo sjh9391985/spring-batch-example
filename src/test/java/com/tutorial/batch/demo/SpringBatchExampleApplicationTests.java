81e55000f4cb98ff7d5c86bd2ba86ad62b349ae2
sjh9391985 <sjh9391985@gmail.com>
2023-06-19 16:47:24 +0900

    feat: "Spring batch, schedulers test"

---
+++ src/test/java/com/tutorial/batch/demo/SpringBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.batch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
